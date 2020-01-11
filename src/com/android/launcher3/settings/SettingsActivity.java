3eb02249653cb50c0a1726d00c56a0e8f6189731
Amir Zaidi <azaidi@live.nl>
2020-01-11 13:55:58 +0100

    [SQUASHED] Launcher3: Add Icon Pack Support from Shade Launcher
    @DennySPb: port to A11, A12-L, A13
    
    Change-Id: Id81da397d5ef33577d6f53f349080c49a61591ab
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    -----
    
    Launcher3: Make icon pack support themed icons
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    Launcher3: Switch to icon pack theming preview
    
    Ref:
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/8155a4f3db5888b578aecb8904529a22f962968e
    https://github.com/crdroidandroid/android_packages_apps_Launcher3/commit/174d11c6fa88e85920ec078af506fc56ef3bccd4
    
    Tweaked to our usage
    
    Change-Id: Ieb7a62c8df1ac0024e1238fec14d4d3c2ed5d38b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    -----
    
    Launcher3: Remove repeated strings in app info
    
    Change-Id: I97fc8fc2277e8be912ea33d3f5e346eb72b95c47
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    -----
    
    Launcher3: Use proper icon provider in LauncherAppState
    
    Change-Id: I1521995a7d98c147e394cf2834b8e9bc93e74b26
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    -----
    
    Launcher: Bottomsheet: Don't make package source pref clickable
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    -----
    
    InfoBottomSheet: Fix NPE with AppTransitionManager
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I03d5490b62c063e85c21a6c90301dee24b9b1871
    
    -----
    
    Launcher3: Do not attempt to show promise icon when info is null
    
    @neobuddy89: Adapted for A12.
    
    Change-Id: I9b7a4f1923194657f7fd5b0ad75fc095a072086b
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: StarkDroid <gamerprince.exp@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -23,10 +23,13 @@ import static com.android.launcher3.states.RotationHelper.ALLOW_ROTATION_PREFERE
 
 import static com.android.launcher3.OverlayCallbackImpl.KEY_ENABLE_MINUS_ONE;
 
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -54,6 +57,8 @@ import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
+import com.android.launcher3.customization.IconDatabase;
+import com.android.launcher3.icons.pack.IconPackSettingsActivity;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.model.WidgetsModel;
 import com.android.launcher3.states.RotationHelper;
@@ -190,7 +195,8 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
     /**
      * This fragment shows the launcher preferences.
      */
-    public static class LauncherSettingsFragment extends PreferenceFragmentCompat {
+    public static class LauncherSettingsFragment extends PreferenceFragmentCompat implements
+            SharedPreferences.OnSharedPreferenceChangeListener {
 
         private String mHighLightKey;
         private boolean mPreferenceHighlighted = false;
@@ -215,6 +221,20 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
             getPreferenceManager().setSharedPreferencesName(LauncherFiles.SHARED_PREFERENCES_KEY);
             setPreferencesFromResource(R.xml.launcher_preferences, rootKey);
 
+            updatePreferences();
+
+            Utilities.getPrefs(getContext())
+                    .registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onDestroyView () {
+            Utilities.getPrefs(getContext())
+                .unregisterOnSharedPreferenceChangeListener(this);
+            super.onDestroyView();
+        }
+
+        private void updatePreferences() {
             PreferenceScreen screen = getPreferenceScreen();
             for (int i = screen.getPreferenceCount() - 1; i >= 0; i--) {
                 Preference preference = screen.getPreference(i);
@@ -261,6 +281,15 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
             outState.putBoolean(SAVE_HIGHLIGHTED_KEY, mPreferenceHighlighted);
         }
 
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+            switch (key) {
+                case IconDatabase.KEY_ICON_PACK:
+                    updatePreferences();
+                    break;
+            }
+        }
+
         protected String getParentKeyForPref(String key) {
             return null;
         }
@@ -296,6 +325,10 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                 case KEY_ENABLE_MINUS_ONE:
                     mShowGoogleAppPref = preference;
                     updateIsGoogleAppEnabled();
+                    return true;
+
+                case IconDatabase.KEY_ICON_PACK:
+                    setupIconPackPreference(preference);
                     return true;
             }
 
@@ -384,5 +417,18 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                 }
             });
         }
+
+        private void setupIconPackPreference(Preference preference) {
+            final String pkgLabel = IconDatabase.getGlobalLabel(getActivity());
+            preference.setSummary(pkgLabel);
+            preference.setOnPreferenceClickListener(p -> {
+                startActivity(new Intent(getActivity(), IconPackSettingsActivity.class));
+                return true;
+            });
+        }
+    }
+
+    public interface OnResumePreferenceCallback {
+        void onResume();
     }
 }
