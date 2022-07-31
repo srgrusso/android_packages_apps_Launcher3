214e162753886367c5feb41bb8b6e016c3a6f1aa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-31 23:43:51 +0530

    Launcher3: Add a toggle for smartspace
    Change-Id: I3b2681d51197642ba0957111db455937c0c376e8

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -154,6 +154,7 @@ public final class Utilities {
     public static final String LENS_ACTIVITY = "com.google.android.apps.lens.MainActivity";
     public static final String LENS_URI = "google://lens";
     public static final String LENS_SHARE_ACTIVITY = "com.google.android.apps.search.lens.LensShareEntryPointActivity";
+    public static final String KEY_SMARTSPACE = "pref_smartspace";
 
     /**
      * Returns true if theme is dark.
@@ -801,4 +802,14 @@ public final class Utilities {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DOCK_SEARCH, true);
     }
+
+    public static boolean showSmartspace(Context context) {
+        return isGSAEnabled(context) && isSmartspaceEnabled(context);
+    }
+
+    private static boolean isSmartspaceEnabled(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_SMARTSPACE, true);
+    }
+
 }
